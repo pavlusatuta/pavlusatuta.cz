247a7014490d3431d863bd55074d740ca5b1b35f
pavlusatuta <ppavluss@gmail.com>
2026-07-05 17:07:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,12 +18,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const days = Math.floor(diff / (1000 * 60 * 60 * 24));
         const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
         const minutes = Math.floor((diff / (1000 * 60)) % 60);
-        const seconds = Math.floor((diff / 1000) % 60);
 
         document.getElementById("days").textContent = days;
         document.getElementById("hours").textContent = hours;
         document.getElementById("minutes").textContent = minutes;
-        document.getElementById("seconds").textContent = seconds;
     }
 
     updateCountdown();
